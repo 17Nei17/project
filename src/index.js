91dda031fe7205ce05c9e5ac2f497b2d80da00e7
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2019-12-19 12:43:38 +0300

    настроила проект

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import './styles/main.css';
+/*
+const modal = document.getElementById('mymodal');
+const btn = document.getElementById('btn_modal_window');
+const close = document.getElementById('close_modal_window');
+console.log(close);
+btn.onclick = function() {
+  modal.style.display = 'block';
+};
+close.onclick = function() {
+  modal.style.display = 'none';
+};
+window.onclick = function(event) {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+};
+*/
