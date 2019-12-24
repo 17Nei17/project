afefedda8d0653a2fef6516096a7db20f86ea35b
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2019-12-24 14:28:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import './styles/main.css';
-/*
-const modal = document.getElementById('mymodal');
-const btn = document.getElementById('btn_modal_window');
-const close = document.getElementById('close_modal_window');
-console.log(close);
-btn.onclick = function() {
-  modal.style.display = 'block';
-};
-close.onclick = function() {
-  modal.style.display = 'none';
-};
-window.onclick = function(event) {
-  if (event.target === modal) {
-    modal.style.display = 'none';
-  }
-};
-*/
+import Model from './model';
+import View from './view';
+import Controller from './controller';
+import wishlist from './wishlist';
+import { save, load } from './helpers';
+
+const state = load();
+
+const model = new Model(state || undefined);
+model.on('change', state => save(state));
+
+const view = new View();
+const controller = new Controller(model, view);
