d1c517d30f96601565a2954ffa1625e68352dcb5
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2019-12-28 14:17:14 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
 import './styles/main.css';
-import Model from './model';
-import View from './view';
-import Controller from './controller';
-import wishlist from './wishlist';
-import { save, load } from './helpers';
 
-const state = load();
+import { ListModel, BooksModel } from './model';
 
-const model = new Model(state || undefined);
-model.on('change', state => save(state));
+import ListView from './Listview';
+import BookView from './BooksView';
+// import EndView from './EndView';
 
-const view = new View();
-const controller = new Controller(model, view);
+import { ListController, BookController } from './controller';
+
+const listModel = new ListModel();
+const booksModel = new BooksModel();
+// const endModel = new EndModel();
+
+const bookView = new BookView();
+// const endView = new EndView();
+const listview = new ListView();
+const listController = new ListController(listModel, listview); // предпологаю что они и не нужны
+const bookController = new BookController(booksModel, listview, bookView);
+// const endController = new EndController(endModel, listview, bookView, endView); // предпологаю что они и не нужны
+
+const beginBooks = [
+  'Гарри Поттер и Кубок огня',
+  'Властелин колец 2 том: Две крепости ',
+  'Превращение',
+  'Война и мир',
+];
+
+function start(arrBook) {
+  arrBook.forEach(elem => {
+    listController.addDrawing(elem);
+  });
+}
+
+start(beginBooks);
