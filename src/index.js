f4a12e8223586d68a6cd9a43cae17d819627c85d
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2020-01-02 15:06:49 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,29 +7,32 @@ import BookView from './BooksView';
 // import EndView from './EndView';
 
 import { ListController, BookController } from './controller';
-
-const listModel = new ListModel();
-const booksModel = new BooksModel();
 // const endModel = new EndModel();
+import { save, load } from './helpers';
+
+// const listModel = new ListModel();
+const booksModel = new BooksModel();
+const State = load();
+const listModel = new ListModel(State || undefined);
+listModel.on('change', State => save(State));
 
 const bookView = new BookView();
 // const endView = new EndView();
 const listview = new ListView();
-const listController = new ListController(listModel, listview); // предпологаю что они и не нужны
-const bookController = new BookController(booksModel, listview, bookView);
-// const endController = new EndController(endModel, listview, bookView, endView); // предпологаю что они и не нужны
-
+const listController = new ListController(listModel, listview); // предполагаю что они и не нужны
+const bookController = new BookController(booksModel, bookView, listModel);
+// const endController = new EndController(endModel, listview, bookView, endView); //
 const beginBooks = [
-  'Гарри Поттер и Кубок огня',
-  'Властелин колец 2 том: Две крепости ',
-  'Превращение',
-  'Война и мир',
+  ['Властелин колец, братство кольца', 'Толкиен', 'Книга о кольце'],
+  ['Три товарища', 'Эрих Мария Ремарк', 'Книга'],
+  ['Мастер и Маргарита', 'Михаил Афанасьевич Булгаков', 'Книга'],
+  ['Маленький принц', 'Антуан де Сент-Экзюпери', 'Книга'],
+  ['Трудно быть Богом', 'братья Стругацкие', 'Книга'],
 ];
 
 function start(arrBook) {
   arrBook.forEach(elem => {
-    listController.addDrawing(elem);
+    listController.addBook(elem);
   });
 }
-
 start(beginBooks);
