0f3d8e6f58468785bc2d9c93c4c9ff95d5da3cee
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2020-01-03 17:36:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,24 +4,19 @@ import { ListModel, BooksModel } from './model';
 
 import ListView from './Listview';
 import BookView from './BooksView';
-// import EndView from './EndView';
 
 import { ListController, BookController } from './controller';
-// const endModel = new EndModel();
 import { save, load } from './helpers';
 
-// const listModel = new ListModel();
 const booksModel = new BooksModel();
 const State = load();
 const listModel = new ListModel(State || undefined);
 listModel.on('change', State => save(State));
 
 const bookView = new BookView();
-// const endView = new EndView();
 const listview = new ListView();
 const listController = new ListController(listModel, listview); // предполагаю что они и не нужны
 const bookController = new BookController(booksModel, bookView, listModel);
-// const endController = new EndController(endModel, listview, bookView, endView); //
 const beginBooks = [
   ['Властелин колец, братство кольца', 'Толкиен', 'Книга о кольце'],
   ['Три товарища', 'Эрих Мария Ремарк', 'Книга'],
