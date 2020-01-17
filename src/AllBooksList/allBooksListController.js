d33e254d8be11b14c3ac49bf6d377e7fedcc57d2
17Nei17 <52545896+17Nei17@users.noreply.github.com>
2020-01-17 16:51:42 +0300

    Create allBooksListController.js

---
+++ src/AllBooksList/allBooksListController.js
@@ -0,0 +1,55 @@
+// controller
+class AllBooksListController {
+  constructor(model, view) {
+    this.model = model;
+    this.view = view;
+
+    view.on('add', this.addBookOnListController.bind(this));
+    view.on('remove', this.removeBook.bind(this));
+    view.on('draging', this.dragBook.bind(this));
+    view.on('editName', this.editName.bind(this));
+    //   view.on('getObject', this.getObject.bind(this));
+
+    view.show(model.items);
+  }
+
+  /*
+  getObject(name) {
+    return this.model.getItemByName(name);
+  }
+*/
+  addBookOnListController(book) {
+    let newID = 0;
+    if (this.model.items.length > 4) {
+      newID = Date.now();
+    } else {
+      newID = Date.now() + this.model.items.length;
+    }
+
+    const item = this.model.addItem({
+      id: newID,
+      title: book.title,
+      author: book.author,
+      description: book.description,
+      completed: false,
+    });
+    this.view.addItem(item);
+  }
+
+  removeBook(id) {
+    this.model.removeItem(id);
+    this.view.removeItem(id);
+  }
+
+  dragBook(draw) {
+    this.craftingview.usingDraw.textContent = draw;
+  }
+
+  editName({ id, title, author }) {
+    const item = this.model.updateItem(id, { title }, { author });
+    this.view.editItem(item);
+  }
+}
+
+console.log('ListController ok');
+export default AllBooksListController;
